b265eb7e8cec8ccdcc48e0d73218b3f5110f5dbe
PENKE-JNANESWARI <jnanipallavi@gmail.com>
2022-08-23 19:39:34 +0530

    files added

--- jnani.php
+++ jnani.php
@@ -1,18 +1,48 @@
-<html>
-<body>
-<form action="jnani1.php">
-<label>subject</label>
-            <select>
-                
-                <option>WT</option>
-                <option>DBMS</option>
-                <option>COA</option>
-                <option>CD</option>
-                <option>DATA SCIENCE</option>
-            </select><br><br>
-            <textarea rows="5" cols="20"> Enter your question</textarea><br><br>
-            <input type="submit" value="Submit">Submit
- 
-</form>
-</body>
-<html>
+<!doctype html>
+<html lang ="en">
+    <head>
+        <meta charset="utf-08">
+        <meta name ="viewport" content="width= device width initial-scale=1";>
+        <title>query mode</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"> 
+        <style>
+            .container{
+                margin-top: 100px;
+            }
+            input[type='submit']{
+                background-color: rgb(0, 113, 128);
+                color: white;
+                width: 100px;
+                height: 40px;
+                font-size: 20px;
+            }
+            input[type='submit']:hover{
+                opacity: 0.8;
+                box-shadow: 0px 0px 10px rgb(0, 242, 177);
+            }
+            body{
+                background-color:wheat;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <form action="jnani1.html">
+                <center>
+            <label>Subjects</label>
+                        <select>
+                            <option>Web Technologies</option>
+                            <option>DATABASE</option>
+                            <option>COMPILER DESIGN</option>
+                            <option>MACHINE LEARNING</option>
+                            <option>DATA SCIENCE</option>
+                        </select><br><br>
+                        <textarea rows="13" cols="80"> </textarea><br><br>
+                        <textarea rows="6" cols="25"name="question"> Enter your question here...</textarea><br><br>
+                        <input type="submit" value="Submit">
+                    </center>
+            
+            </form>
+            
+        </div>
