44ea8cf6169fe30324753fa7ec8f1f97a46c8134
PENKE-JNANESWARI <jnanipallavi@gmail.com>
2022-08-23 19:04:40 +0530

    files added

---
+++ jnani.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<form action="jnani1.php">
+<label>subject</label>
+            <select>
+                
+                <option>WT</option>
+                <option>DBMS</option>
+                <option>COA</option>
+                <option>CD</option>
+                <option>DATA SCIENCE</option>
+            </select><br><br>
+            <textarea rows="5" cols="20"> Enter your question</textarea><br><br>
+            <input type="submit" value="Submit">Submit
+ 
+</form>
+</body>
+<html>
